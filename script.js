2a4b86a757bfda7fb2a42e5104a48593e1639ba2
11scorpion20 <139935147+11scorpion20@users.noreply.github.com>
2023-07-18 19:24:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let car1 = {};
+
+let car2 = {
+    imgUrl: './images/civic.webp',
+    make: 'Honda',
+    model: 'Civic',
+    year: 2023,
+    color: 'silver',
+    features: ['bluetooth', 'backup camera', 'leather seats']
+}
